4d9034d320e799c43c5c51db8c4c5285ee5a9379
Mostafa Tarek <88462067+Mostafa123165@users.noreply.github.com>
2022-09-03 03:23:07 +0200

    Implemation data structures

---
+++ main.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int MaxSize = 100 ;
+template< class  t>
+class  Stack {
+    int top ;
+    int a[MaxSize] ;
+
+public:
+    Stack():top(-1){}
+
+    bool isEmpty(){
+        if(top < 0) return true ;
+        else return false ;
+    }
+
+    void pop(){
+        if(!isEmpty()) top--;
+        else {
+            cout<<"Stack is empty! \n" ;
+        }
+    }
+
+    void getTop(){
+        if(!isEmpty()) cout<<a[top]<<'\n' ;
+        else {
+            cout<<"Stack is empty! \n" ;
+        }
+    }
+
+    void push(t Element){
+        top++ ;
+        a[top] = Element ;
+    }
+
+    void print(){
+        for(int i =top ;i>=0 ;i--){
+            cout<<a[i]<<" ";
+        }cout<<'\n' ;
+    }
+};
+int main() {
+Stack <int>s ;
+s.push(5) ;
+s.push(6) ;
+s.push(7) ;
+s.push(8) ;
+s.print();
+s.getTop();
+s.pop();
+s.getTop();
+}
